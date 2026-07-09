daa28a7e511e84059ad5adf50020e5f031f0e7ee
AbubakarFarooqi <azanalifarooqi@gmail.com>
2026-07-10 04:47:53 +0500

    10 july 2026

--- Ahsan/Coding-Class-Codes/Web Development/All About Javascript/weather API/script.js
+++ Ahsan/Coding-Class-Codes/Web Development/All About Javascript/weather API/script.js
@@ -4,3 +4,26 @@ const cityCountrySpan = document.getElementById("cityName")
 const tempCelSpan = document.getElementById("temp_cels_number")
 const tempFahSpan = document.getElementById("temp_fahr_number")
 const windSpeedSpan = document.getElementById("wind_speed_number")
+
+findWeatherButton.addEventListener('click',async ()=>{
+    let city = citySelect.value
+    let apiUrl = `https://api.weatherapi.com/v1/current.json?key=0f9f65342419461e869235451260607&q=${city}`
+
+    let response = await fetch(apiUrl) // whenever working with api you have to do await
+
+    if (!response.ok){
+        alert("Api failed")
+    }
+
+    let data = await response.json()
+
+    let country = data.location.country
+    let temp_c = data.current.temp_c
+    let temp_f = data.current.temp_f
+    let wind_kph = data.current.wind_kph
+
+    cityCountrySpan.innerHTML = `${city}, ${country}`
+    tempCelSpan.innerHTML = `${temp_c}`
+    tempFahSpan.innerHTML = `${temp_f}`
+    windSpeedSpan.innerHTML = `${wind_kph}`
+})
---
+++ stars acadmey/function.py
@@ -0,0 +1,15 @@
+# def natural_numbers():
+#     i=1
+#     while i<=10:
+#         print(i)
+#         i=i+1
+# natural_numbers()
+
+def add_num(a,b):
+    res = a+b
+    return res
+
+num1 = int(input("enter a number 1-> "))
+num2 = int(input("enter a number 2-> "))
+sum = add_num(num1,num2)
+print(sum)
